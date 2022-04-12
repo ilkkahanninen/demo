359dae5ca23937da9336337b78518e6c4d14ea4b
Ilkka Hänninen <ilkka.hanninen@gmail.com>
2022-04-12 18:06:07 +0300

    First raymarching step, fix error messages

--- src/webgl.ts
+++ src/webgl.ts
@@ -14,8 +14,8 @@ export function initShader(
   vsSource: string,
   fsSource: string
 ): WebGLProgram {
-  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource)!;
-  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource)!;
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
 
   const shaderProgram = gl.createProgram()!;
   gl.attachShader(shaderProgram, vertexShader);
@@ -41,12 +41,12 @@ export function loadShader(
   gl.shaderSource(shader, source);
   gl.compileShader(shader);
 
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+  const info = gl.getShaderInfoLog(shader);
+
+  if (info?.length) {
     gl.deleteShader(shader);
-    console.warn(source);
-    throw (
-      "An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader)
-    );
+    console.error(info);
+    throw "An error occurred compiling the shaders";
   }
 
   return shader;
